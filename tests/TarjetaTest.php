3a389c024f67718dd69e7665166cb5148eb084f0
Lautaro Martin <31867574+Albertonisman@users.noreply.github.com>
2017-10-13 14:33:14 -0300

    Update TarjetaTest.php

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -11,17 +11,27 @@ class EstacionTest extends TestCase {
      */
     public function testSaldoCero() {
         $tarjeta = new Tarjeta;
-        $tarjeta->saldotarjeta=0;
         $tarjeta->saldo(50);
         $this->assertEquals($tarjeta->saldotarjeta, 50);
+        //Reinicio la carga en la tarjeta para simplificar los test
         $tarjeta->saldotarjeta=0;
         $tarjeta->saldo(332);
         $this->assertEquals($tarjeta->saldotarjeta,388);
         $tarjeta->viaje(145);
         $this->assertEquals($tarjeta->saldotarjeta,388-9.75);
+        //Hago un segundo viaje en el mimso colectivo
         $tarjeta->viaje(145);
         $this->assertEquals($tarjeta->saldotarjeta,388-19.50);
+        //Ahora hago un transbordo
         $tarjeta->viaje(133);
+        $this->assertEquals($tarjeta->saldotarjeta,388-22.70);
+        //Reinicio la carga en la tarjeta para simplificar los test
+        $tarjeta->saldotarjeta=0;
+        //Ahora hacemos 1 viaje un transbordo y pagar un viaje a otra persona (9.75+ 3.2 + 9.75)
+        $tarjeta->saldo(332);
+        $tarjeta->viaje(139);
+        $tarjeta->viaje(145);
+        $tarjeta->viaje(145);
         $this->assertEquals($tarjeta->saldotarjeta,388-22.70);
     }
 }
