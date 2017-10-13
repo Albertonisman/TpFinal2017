a74a301b0147033ce7adc0c8990c7cec457773a6
Lautaro Martin <31867574+Albertonisman@users.noreply.github.com>
2017-10-13 14:05:16 -0300

    Update TarjetaTest.php

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -10,19 +10,19 @@ class EstacionTest extends TestCase {
      * Comprueba que el saldo de una tarjeta nueva sea cero.
      */
     public function testSaldoCero() {
-        $tarjeta = new Tarjeta;
         $tarjeta->saldotarjeta=0;
     }
-        $tarjeta->saldo(50);
-        $this->assertEquals($tarjeta->saldotarjeta, 50);
-        $tarjeta->saldotarjeta=0;
-        $tarjeta->saldo(332)
-        $this->assertEquals($tarjeta->saldotarjeta,388);
-        $tarjeta->viaje(145);
-        $this->asserEquals($tarjeta->saldotarjeta,388-9.75);
-        $tarjeta->viaje(145);
-        $this->asserEquals($tarjeta->saldotarjeta,388-19.50);
-        $tarjeta->viaje(133);
-        $this->asserEquals($tarjeta->saldotarjeta,388-22.70);
+    $tarjeta = new Tarjeta;
+    $tarjeta->saldo(50);
+    $this->assertEquals($tarjeta->saldotarjeta, 50);
+    $tarjeta->saldotarjeta=0;
+    $tarjeta->saldo(332)
+    $this->assertEquals($tarjeta->saldotarjeta,388);
+    $tarjeta->viaje(145);
+    $this->asserEquals($tarjeta->saldotarjeta,388-9.75);
+    $tarjeta->viaje(145);
+    $this->asserEquals($tarjeta->saldotarjeta,388-19.50);
+    $tarjeta->viaje(133);
+    $this->asserEquals($tarjeta->saldotarjeta,388-22.70);
         
 }
