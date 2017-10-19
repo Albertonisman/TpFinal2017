f0385d253c9e4dd76b03bdf5b1cbbd9f26863a9b
Francisco Gradndin <franciscograndin@hotmail.com>
2017-10-18 23:42:17 -0300

    Agrega parametro fecha y hora clase Viaje.

--- src/Viaje.php
+++ src/Viaje.php
@@ -3,22 +3,22 @@
 namespace tpFinal;
 
 class Viaje {
-    protected $tipo;
+    protected $tipo; //Colectivo o bici
     protected $monto;
     protected $trasporte;
-    
+    protected $fecha_y_hora;
     public function obtener_tipo() {
        return $this->tipo;
     }
-    
     public function obtener_monto() {
        return $this->monto;
     }
-    
     public function obtener_transporte() {
        return $this->transporte;
     }
-    
+    public function obtener_fecha_y_hora() {
+        return $this->fecha_y_hora;
+    }
     public function __construct ($tipo, $monto, $transporte) {
         $this->tipo=$tipo;
         $this->monto=$monto;
