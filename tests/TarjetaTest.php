8a00c71039c43cbe649cf3aa96ecc7037a40778a
Nismansomostodos <franciscograndin@hotmail.com>
2017-10-20 10:50:43 -0300

    Organiza los test en varias funciones.

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -9,41 +9,46 @@ class EstacionTest extends TestCase {
     /**
      * Comprueba que el saldo de una tarjeta nueva sea cero.
      */
-    public function testSaldoCero() {
+    public function testCargarSaldo() {
         $tarjeta = new Tarjeta;
         $tarjeta->cargar_saldo(50);
         $this->assertEquals($tarjeta->saldotarjeta(), 50);
         $tarjeta->reiniciosaldo();
-        $tarjeta->cargar_saldo(624);
-        $this->assertEquals($tarjeta->saldotarjeta(),776);
-        $tarjeta->reiniciosaldo();
-        //Reinicio la carga en la tarjeta para simplificar los test
         $tarjeta->cargar_saldo(332);
         $this->assertEquals($tarjeta->saldotarjeta(),388);
+        $tarjeta->reiniciosaldo();
+        $tarjeta->cargar_saldo(624);
+        $this->assertEquals($tarjeta->saldotarjeta(),776);
+    }
+    public function testViaje() {
+        $tarjeta = new Tarjeta;
+        $tarjeta->cargar_saldo(100);
         $tarjeta->viaje(145);
-        $this->assertEquals($tarjeta->saldotarjeta(),388-9.75);
+        $this->assertEquals($tarjeta->saldotarjeta(),100-9.75);
         //Hago un segundo viaje en el mimso colectivo
         $tarjeta->viaje(145);
-        $this->assertEquals($tarjeta->saldotarjeta(),388-19.50);
+        $this->assertEquals($tarjeta->saldotarjeta(),100-19.50);
         //Ahora hago un transbordo
         $tarjeta->viaje(133);
-        $this->assertEquals($tarjeta->saldotarjeta(),388-22.70);
+        $this->assertEquals($tarjeta->saldotarjeta(),100-22.70);
         //Reinicio la carga en la tarjeta para simplificar los test
         $tarjeta->reiniciosaldo();
         //Ahora hacemos 1 viaje un transbordo y pagar un viaje a otra persona (9.75+ 3.2 + 9.75)
-        $tarjeta->cargar_saldo(332);
+        $tarjeta->cargar_saldo(100);
         $tarjeta->viaje(139);
         $tarjeta->viaje(145);
         $tarjeta->viaje(145);
-        $this->assertEquals($tarjeta->saldotarjeta(),388-22.70);
+        $this->assertEquals($tarjeta->saldotarjeta(),100-22.70);
         //Reinicio el saldo
-        $tarjeta->reiniciosaldo();
-        $tarjeta->cargar_saldo(332);
+    }
+    public function testAlquileBici() {
+        $tarjeta = new Tarjeta;
+        $tarjeta->cargar_saldo(100);
         //Alquilo una bici
         $tarjeta->alquilerbici();
-        $this->assertEquals($tarjeta->saldotarjeta(),388-14.625);
+        $this->assertEquals($tarjeta->saldotarjeta(),100-14.625);
         $tarjeta->alquilerbici();
-        $this->assertEquals($tarjeta->saldotarjeta(),388-14.625);
+        $this->assertEquals($tarjeta->saldotarjeta(),100-14.625);
         
     }
 }
