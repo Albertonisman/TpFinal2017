9cc70cc96474c5f31606f074ab3656f75d75b0f8
Francisco Grandin <31454485+Nismansomostodos@users.noreply.github.com>
2017-10-31 16:25:10 -0300

    Arregla

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -64,18 +64,35 @@ class Tarjeta {
                         print ("No puede realizar el viaje"); //no se me ocurre otra forma de hacerlo mejor si no pude realizar el viaje 
                     }
                 }
-                
                 $this->ult_colectivo = $Transporte;
             }
             else {
-                if( $this->ult_colectivo->obtener_linea() != $Transporte->obtener_linea() && ($fecha-$this->dia_colectivo)<3600 ) {
+                if($this->ult_colectivo->obtener_linea() != $Transporte->obtener_linea() && ($fecha-$this->dia_colectivo)<3600 ) {
                     $this->saldo = $this->saldo - 3.20;
                     array_unshift($this->viajes_realizados, new Viaje("Trasbordo", 3.20, $Transporte->obtener_linea(),$f));
                     $this->ult_colectivo = $Transporte;
                 }
                 else {
-                    $this->saldo = $this->saldo - 9.75;
-                    array_unshift(($this->viajes_realizados), new Viaje("Normal", 9.75, $Transporte->obtener_linea(),$f)); 
+                    if($this->plus > 0) {
+                        $this->saldo = $this->saldo - (9.75 + 9.75*$this->plus) ;
+                        array_unshift(($this->viajes_realizados), new Viaje("Normal", 9.75+9.75*$this->plus, $Transporte->obtener_linea()),$f); 
+                    } 
+                    else {
+                        $this->saldo = $this->saldo - 9.75;
+                        array_unshift(($this->viajes_realizados), new Viaje("Normal", 9.75, $Transporte->obtener_linea(),$f)); 
+                    }
+                }
+                else {
+                    if($this->plus <= 2) {
+                        $this->plus = $this->plus + 1;
+                        array_unshift(($this->viajes_realizados), new Viaje("Plus " .  ($this->plus+1), 9.75, $Transporte->obtener_linea(),$f));
+                    }
+                    else {
+                        print ("No puede realizar el viaje"); //no se me ocurre otra forma de hacerlo mejor si no pude realizar el viaje 
+                    }
+                }
+                
+                $this->ult_colectivo = $Transporte;
                 }
             }
             $this->dia_colectivo = $fecha;    
